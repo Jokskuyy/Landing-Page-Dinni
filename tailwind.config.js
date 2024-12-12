5900a6f8eb0f65f149be0c418e98c567c26af8e6
Jokskuyy <160201837+Jokskuyy@users.noreply.github.com>
2024-12-13 05:45:58 +0700

    Prototype - 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,49 @@ module.exports = {
     './index.html',        // File HTML utama
     './**/*.html',         // Semua file HTML di folder
     './src/**/*.{js,ts,jsx,tsx,vue}', // File JS/TS/Vue jika Anda menggunakan framework
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
+      animation: {
+        'fade-in-left': 'fadeInLeft 1s ease-out',
+        'fade-in-right': 'fadeInRight 1s ease-out',
+        'fade-in-top': 'fadeInTop 1s ease-out',
+        'fade-in-bottom': 'fadeInBottom 1s ease-out',
+      },
+      keyframes: {
+        fadeInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInTop: {
+          '0%': { opacity: '0', transform: 'translateY(-100%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInBottom: {
+          '0%': { opacity: '0', transform: 'translateY(100%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
       colors: {
-        "bookmark-purple": "#5267DF",
-        "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242A45",
-        "bookmark-grey": "#9194A2",
         "bookmark-white": "#f7f7f7",
-        "ld_yellow": "#FFD929",
+        "ld-yellow": "#FFD929",
         "ld-green": "#006B7B",
-        "ld-white": "#FAF9F6", 
+        "ld-white": "#FAF9F6",
       }
     },
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
-      Inter: ["Inter", "sans-serif"]
-    },
+      Inter: ["Inter", "sans-serif"],
+      alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
     container: {
       center: true,
       padding: '1rem',
@@ -32,6 +57,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
-
