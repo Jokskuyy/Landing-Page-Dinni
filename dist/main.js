ed20df463bf702191507e4a8ee1ce27c0461868a
Jokskuyy <160201837+Jokskuyy@users.noreply.github.com>
2024-12-28 14:54:10 +0700

    preloader

--- dist/main.js
+++ dist/main.js
@@ -16,29 +16,9 @@ copyEmailButton.addEventListener('click', function () {
     alert('Email berhasil disalin ke clipboard!');
 });
 
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-
-        if (targetElement) {
-            let offset = 0;
-
-            if (targetId === '#aboutme') offset = -40;
-            if (targetId === '#pricing') offset = -20;
-            if (targetId === '#artikel') offset = -30;
-            if (targetId === '#contact') offset = 400;
-            if (targetId === '#form') offset = 40000;
-
-            const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
-            const offsetPosition = elementPosition + offset;
-
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
+window.onload = function () {
+    const preloader = document.getElementById("preloader");
+    if (preloader) {
+        preloader.style.display = "none";
+    }
+};
